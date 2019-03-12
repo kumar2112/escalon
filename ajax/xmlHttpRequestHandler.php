515232bdb0720b51e61c17a9692ea43f4c423d73
John Marcus <marcussjohnn@gmail.com>
2019-03-12 22:00:10 +0530

    Update xmlHttpRequestHandler.php

--- ajax/xmlHttpRequestHandler.php
+++ ajax/xmlHttpRequestHandler.php
@@ -1,8 +1,18 @@
 <?php 
    require_once 'Model/Skills.php';
-   
-     
    $category_id=$_POST['cat_id'];
+   $skills=Skills::getSkillByCategory($category_id);
+   
+   $html='<div class='row'>';
+   $i=0;
+   foreach($skills as $skill){
+        
+        $html.='<div class="col-md-3">';
+        $html.='<label>'.$skill['name'].'</label>';
+        $html.='<input type="range" name="points" min="1" max="5" name="evalauate_range[]" id="evalauate_range_'.$i.'" >';
+        $html.='<div/>';
+   }
+   $html.='<div/>';
 
-  $skills=Skills::get
+   echo $html;
 ?>
